302b61ceaa0173aa32945e6a3b6d3bfc720bb70e
anoo52sha-creator <anoo52sha@gmail.com>
2026-03-04 17:22:54 +0530

    remove broken unref line

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
-import { setTimeout } from 'node:timers';
-setTimeout.unref();
+
 
 dotenv.config();
 const { Pool } =  pg;
